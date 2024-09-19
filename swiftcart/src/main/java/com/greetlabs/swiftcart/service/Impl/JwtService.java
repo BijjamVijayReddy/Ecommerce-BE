13b5de5e1016a62f73962bda9854930f6396e8a2
Sivareddy0930 <sivareddy0930@gmail.com>
2024-09-19 10:58:43 +0530

    added some security config and handling

--- swiftcart/src/main/java/com/greetlabs/swiftcart/service/Impl/JwtService.java
+++ swiftcart/src/main/java/com/greetlabs/swiftcart/service/Impl/JwtService.java
@@ -1,16 +1,17 @@
 package com.greetlabs.swiftcart.service.Impl;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -62,11 +63,11 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .verifyWith(getKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+            return Jwts.parser()
+                    .verifyWith(getKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
 
     }
 
